9fe12d9844c9a1146b2ca4264097a85ec63d93e9
hector <hectorp2710@gmail.com>
2023-03-13 12:44:53 +0100

    La inn testing av sikkerhet

--- src/test/java/oslomet/testing/EnhetstestSikkerhet.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhet.java
@@ -1,21 +1,31 @@
 package oslomet.testing;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.mock.web.MockHttpSession;
-import oslomet.testing.API.BankController;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
     @RunWith(MockitoJUnitRunner.class)
     public class EnhetstestSikkerhet {
 
         @InjectMocks
         // denne skal testes
-        private BankController bankController;
+        private Sikkerhet sikkerhetsController;
 
         @Mock
         // denne skal Mock'es
@@ -23,17 +33,52 @@ import oslomet.testing.Sikkerhet.Sikkerhet;
 
         @Mock
         // denne skal Mock'es
-        private MockHttpSession session;
+        MockHttpSession session;
 
-        @Test
-        public void sjekkLoggInn(){
-        // arange
+        @Before
+        public void initSession() {
+            Map<String, Object> attributes = new HashMap<String, Object>();
 
+            doAnswer(new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    String key = (String) invocation.getArguments()[0];
+                    return attributes.get(key);
+                }
+            }).when(session).getAttribute(anyString());
+
+            doAnswer(new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    String key = (String) invocation.getArguments()[0];
+                    Object value = invocation.getArguments()[1];
+                    attributes.put(key, value);
+                    return null;
+                }
+            }).when(session).setAttribute(anyString(), any());
         }
 
         @Test
-        public void loggUt(){
+        public void test_sjekkLoggInn(){
+        // arange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("OK");
+        // act
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901", "HeiHeiHei");
+        // assert
+        assertEquals("OK", resultat);
+        }
 
+        @Test
+        public void test_loggUt(){
+            //arrange
+            session.setAttribute("Innlogget", null);
+            sikkerhetsController.loggUt();
+
+            //act
+            String resultat = sikkerhetsController.loggetInn();
+
+            //assert
+            assertNull(resultat);
         }
 
         @Test
